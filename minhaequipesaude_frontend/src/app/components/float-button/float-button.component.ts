faf9aa935d2901d787301bf4c19233e8e962067b
leandromelolm <leandrosiqueira.pe@gmail.com>
2026-07-28 11:52:53 -0300

    style(fab): altera atributos heiht border-radius para serem atribuíveis

--- minhaequipesaude_frontend/src/app/components/float-button/float-button.component.ts
+++ minhaequipesaude_frontend/src/app/components/float-button/float-button.component.ts
@@ -11,10 +11,21 @@ export class FloatButtonComponent {
   @Output() buttonClick = new EventEmitter<void>();
 
   @Input() bottom: string | number = '24px';
+  @Input() height: string | number = '56px';
+  @Input() borderRadius: string | number = '24px';
 
   get bottomStyle(): string {
     return typeof this.bottom === 'number' ? `${this.bottom}px` : this.bottom;
   }
+
+  get heightStyle(): string {
+    return typeof this.height === 'number' ? `${this.height}px` : this.height;
+  }
+
+  get borderRadiusStyle(): string {
+    return typeof this.height === 'number' ? `${this.height}%` : this.height;
+  }
+
 
   onClick() {
     this.buttonClick.emit();
